63c8f6d2a3293140886819769e2b8762c0ff66cc
Brandon Franks <bgfranks106@icloud.com>
2020-02-04 13:21:18 -0500

    setup the base files and created the store for redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-import Header from './components/Header';
-import AddedFeatures from './components/AddedFeatures';
-import AdditionalFeatures from './components/AdditionalFeatures';
-import Total from './components/Total';
+import Header from './components/Header'
+import AddedFeatures from './components/AddedFeatures'
+import AdditionalFeatures from './components/AdditionalFeatures'
+import Total from './components/Total'
 
 const App = () => {
   const state = {
@@ -13,23 +13,23 @@ const App = () => {
       name: '2019 Ford Mustang',
       image:
         'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
+      features: [],
     },
     additionalFeatures: [
       { id: 1, name: 'V-6 engine', price: 1500 },
       { id: 2, name: 'Racing detail package', price: 1500 },
       { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+      { id: 4, name: 'Rear spoiler', price: 250 },
+    ],
+  }
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
-  };
+  }
 
   const buyItem = item => {
     // dipsatch an action here to add an item
-  };
+  }
 
   return (
     <div className="boxes">
@@ -42,7 +42,7 @@ const App = () => {
         <Total car={state.car} additionalPrice={state.additionalPrice} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
